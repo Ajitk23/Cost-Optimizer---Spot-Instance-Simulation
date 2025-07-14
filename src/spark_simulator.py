46c9ff5390742f522b65d9925c3b1b2035918ca5
Ajit Kumar Sahoo <ajitdatascience23@gmail.com>
2025-07-14 15:25:44 +0530

    spark_simulator has been done

--- src/spark_simulator.py
+++ src/spark_simulator.py
@@ -1,4 +1,33 @@
 # spark_simulator.py
-# This module will simulate Spark jobs(word count, aggregation etc.
+# This module will simulate Spark jobs(word count, aggregation etc.)
 
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode, split
+
+def run_word_count():
+
+    # Creating a SparkSession
+    spark = SparkSession.builder.appName("SpotInstanceSimulation").getOrCreate()
+
+    # Loading sample CSV we have
+    df = spark.read.csv("data/sample_data.csv", header = True, inferSchema = True)
+
+    # Splitting text into words
+    words_df = df.select(
+        explode(split(df.text, " ")).alias("word")
+    )
+
+
+    # Counting Words
+    word_count_df = words_df.groupBy("word").count()
+
+
+    # Result
+    word_count_df.show()
+
+    # Stopping Spark Session
+    spark.stop()
+
+if __name__ == "__main__":
+    run_word_count()
+    
